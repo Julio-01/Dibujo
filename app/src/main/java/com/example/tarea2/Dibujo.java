a89172f9f53e0ce689c1800c7b6547eee513743e
Julio Escobedo <julio_escobedo_01@ciencias.unam.mx>
2024-03-04 12:05:36 -0600

    Initial commit

---
+++ app/src/main/java/com/example/tarea2/Dibujo.java
@@ -0,0 +1,13 @@
+package com.example.tarea2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Dibujo extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
